a0561f08fdfbae748faa21dd01e7c6f10e160494
Venkateswarlu <61453808+somu23pavan@users.noreply.github.com>
2020-05-16 17:39:19 +0530

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,1 +1,9 @@
-print "Hello Wprld"
+JUPYTER NOTEBOOK FOR CAPSTONE PROJECT
+In [1]:
+import pandas as pd
+import numpy as np
+In [2]:
+print("Hello Capstone Project Course!")
+Hello Capstone Project Course!
+In [ ]:
+
